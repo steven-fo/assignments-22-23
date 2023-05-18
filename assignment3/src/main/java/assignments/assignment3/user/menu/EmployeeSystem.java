1f682db2b20959d7779ad3d2287117850d9712d5
steven-fo <stevenfo0711@gmail.com>
2023-05-18 12:40:48 +0700

    Added function addEmployee

--- assignment3/src/main/java/assignments/assignment3/user/menu/EmployeeSystem.java
+++ assignment3/src/main/java/assignments/assignment3/user/menu/EmployeeSystem.java
@@ -61,4 +61,15 @@ public class EmployeeSystem extends SystemCLI {
             System.out.printf("Nota %d : %s \n", i, notaList[i].getNotaStatus());
         }
     }
+
+    public void addEmployee(Employee[] employees) {
+        Member[] result = new Member[employees.length + memberList.length];
+     
+     
+        System.arraycopy(memberList, 0, result, 0, memberList.length);
+        System.arraycopy(employees, 0, result, memberList.length, employees.length);
+     
+        memberList = result;
+     }
+     
 }
